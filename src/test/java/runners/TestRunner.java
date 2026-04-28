566b98b820cf9193f63b497b2af83cb0c1f2bfab
Automationenthus <psdevaki@gmail.com>
2026-04-28 13:24:56 -0400

    Corrected the conflicts

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -5,12 +5,18 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
 		features = {
-			    //"src/test/resources/features/SearchChannel.feature",
-			   // "src/test/resources/features/BookmarkFunctionality.feature",
-			   // "src/test/resources/features/VideoBlockerSetup.feature",
-			    "src/test/resources/features/DownloadFunctionality.feature",
-	},
 
+			    "src/test/resources/features/SearchChannel.feature",
+			    "src/test/resources/features/BookmarkFunctionality.feature",
+
+			    "src/test/resources/features/DownloadFunctionality.feature",
+			    "src/test/resources/features/SubscribeAndUnsubscribe.feature",
+			    "src/test/resources/features/PreferencesOnlyUIValidation.feature",
+			    "src/test/resources/features/VideoBlockerSetup.feature",		  
+			    "src/test/resources/features/WatchedVideoCleaner.feature"
+			  
+	},
+	
     glue = {"stepdefinitions", "hooks"},
     plugin = {
         "pretty",
@@ -18,9 +24,9 @@ import io.cucumber.testng.CucumberOptions;
         "json:target/cucumber.json",
         "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
         "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
-       // "com.aventstack.chaintest.plugins.ChainTestCucumberListener:"
+       
     },
-    //monochrome = true,
+    monochrome = true,
     //publish = true   //Maven is trying to publish the Cucumber report online.
     publish = false 
 )
--- src/test/resources/features/DownloadFunctionality.feature
+++ src/test/resources/features/DownloadFunctionality.feature
@@ -4,11 +4,11 @@ Feature: SkyTube Download Feature
     Given The SkyTube app is launched
     
     Scenario: Verify download option is clickable
-    When the user clicks the three dots symbol in any video 
-    Then the user should see download option and should be clickable
+     When the user clicks the three dots symbol in any video 
+     Then the user should see download option and should be clickable
     
     Scenario: Verify download option triggers download attempt
-  When the user clicks the three dots symbol in any video
-  And the user clicks the download option
-  Then the download error message should be displayed
+      When the user clicks the three dots symbol in any video
+      And the user clicks the download option
+      Then the download error message should be displayed
     
